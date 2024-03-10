076773fbc08e5e29be901f6befddf0e26e02f900
Abhishek <abhisam7007871@gmail.com>
2024-03-10 21:51:02 +0530

    Initial Commit[Electricity billing System]

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -8,7 +8,7 @@ public class Splash extends JFrame {
     Splash(){
         setSize(400,400);
         setVisible(true);
-
+        setLocation(50%, 50%);
     }
     public static void main(String[] args) {
         new Splash();
