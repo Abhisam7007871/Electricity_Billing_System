a6aee0579c89a1e37c88d719ddfe09962567b6a0
Abhishek <abhisam7007871@gmail.com>
2024-03-10 22:21:13 +0530

    FEATURE ADDED[Login/Splash]

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -1,14 +1,31 @@
 package electricity.billing.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Splash extends JFrame {
 
     Splash(){
-        setSize(400,400);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icons/splash/Splash.jpg"));
+        Image imageOne = imageIcon.getImage().getScaledInstance(600,400,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon2 = new ImageIcon(imageOne);
+        JLabel imageLabel = new JLabel(imageIcon2);
+        add(imageLabel);
+
+        setSize(600,400);
+        setLocation(500,300);
         setVisible(true);
-        setLocation(50%, 50%);
+
+        try{
+            Thread.sleep(3000);
+            setVisible(false);
+
+            new Login();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
     public static void main(String[] args) {
         new Splash();
