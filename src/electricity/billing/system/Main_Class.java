d76299129673a57d571a133a0a935a17bca96783
Abhishek <abhisam7007871@gmail.com>
2024-03-13 21:51:40 +0530

    FEATURE ADDED[ Main_Class Created]

---
+++ src/electricity/billing/system/Main_Class.java
@@ -0,0 +1,16 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Main_Class extends JFrame {
+
+    Main_Class(){
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
