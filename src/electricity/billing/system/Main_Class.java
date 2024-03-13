8c7e0126ebf422a6a982a9a4f465f52810491dcf
Abhishek <abhisam7007871@gmail.com>
2024-03-13 22:45:22 +0530

    FEATURE ADDED[ Main_Class ADDED- MENU<INFORMATION<USER]

--- src/electricity/billing/system/Main_Class.java
+++ src/electricity/billing/system/Main_Class.java
@@ -29,12 +29,56 @@ public class Main_Class extends JFrame {
         menu.add(newcustomer);
 
         JMenuItem customerDetails = new JMenuItem("Customer Details");
-        newcustomer.setFont(new Font("monospaced",Font.PLAIN,14));
+        customerDetails.setFont(new Font("monospaced",Font.PLAIN,14));
         ImageIcon customerDetailsImg = new ImageIcon(ClassLoader.getSystemResource("icons/customerDetails.png"));
         Image customerDetailsImage = customerDetailsImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
         customerDetails.setIcon(new ImageIcon(customerDetailsImage));
         menu.add(customerDetails);
 
+        JMenuItem depositDetails = new JMenuItem("Deposit Details");
+        depositDetails.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon depositDetailsImg = new ImageIcon(ClassLoader.getSystemResource("icons/depositdetails.png"));
+        Image depositDetailsImage = depositDetailsImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        depositDetails.setIcon(new ImageIcon(depositDetailsImage));
+        menu.add(depositDetails);
+
+        JMenuItem calculateBill = new JMenuItem("Calculator Bills");
+        calculateBill.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon calculateBillImg = new ImageIcon(ClassLoader.getSystemResource("icons/calculatorbills.png"));
+        Image calculateBillImage = calculateBillImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        calculateBill.setIcon(new ImageIcon(calculateBillImage));
+        menu.add(calculateBill);
+
+
+        JMenu info = new JMenu("Information");
+        info.setFont(new Font("serif",Font.PLAIN,15));
+        menuBar.add(info);
+
+        JMenuItem upinfo = new JMenuItem("Update Information");
+        upinfo.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon upinfoImg = new ImageIcon(ClassLoader.getSystemResource("icons/refresh.png"));
+        Image upinfoImage = upinfoImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        upinfo.setIcon(new ImageIcon(upinfoImage));
+        info.add(upinfo);
+
+        JMenuItem viewinfo = new JMenuItem("View Information");
+        viewinfo.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon viewinfoImg = new ImageIcon(ClassLoader.getSystemResource("icons/information.png"));
+        Image viewinfoImage = viewinfoImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        viewinfo.setIcon(new ImageIcon(viewinfoImage));
+        info.add(viewinfo);
+
+
+        JMenu user = new JMenu("User");
+        user.setFont(new Font("serif",Font.PLAIN,15));
+        menuBar.add(user);
+
+        JMenuItem paybill = new JMenuItem("Pay Bills");
+        paybill.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon paybillImg = new ImageIcon(ClassLoader.getSystemResource("icons/pay.png"));
+        Image paybillImage = paybillImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        paybill.setIcon(new ImageIcon(paybillImage));
+        user.add(paybill);
 
 
         setLayout(new FlowLayout());
