852cb37f2c424daeaeb83c875a0c732f225cd0f0
Abhishek <abhisam7007871@gmail.com>
2024-03-13 22:24:07 +0530

    FEATURE ADDED[ Main_Class Created]

--- src/electricity/billing/system/Main_Class.java
+++ src/electricity/billing/system/Main_Class.java
@@ -1,16 +1,48 @@
 package electricity.billing.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main_Class extends JFrame {
 
     Main_Class(){
         setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icons/ebs.png"));
+        Image image = imageIcon.getImage().getScaledInstance(1530,830,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon2 = new ImageIcon(image);
+        JLabel imageLabel = new JLabel(imageIcon2);
+        add(imageLabel);
+
+        JMenuBar menuBar =  new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu menu = new JMenu("Menu");
+        menu.setFont(new Font("serif",Font.PLAIN,15));
+        menuBar.add(menu);
+
+        JMenuItem newcustomer = new JMenuItem("New Customer");
+        newcustomer.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon customerImg = new ImageIcon(ClassLoader.getSystemResource("icons/newcustomer.png"));
+        Image customerImage = customerImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        newcustomer.setIcon(new ImageIcon(customerImage));
+        menu.add(newcustomer);
+
+        JMenuItem customerDetails = new JMenuItem("Customer Details");
+        newcustomer.setFont(new Font("monospaced",Font.PLAIN,14));
+        ImageIcon customerDetailsImg = new ImageIcon(ClassLoader.getSystemResource("icons/customerDetails.png"));
+        Image customerDetailsImage = customerDetailsImg.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        customerDetails.setIcon(new ImageIcon(customerDetailsImage));
+        menu.add(customerDetails);
+
+
+
+        setLayout(new FlowLayout());
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        new Main_Class();
     }
 }
